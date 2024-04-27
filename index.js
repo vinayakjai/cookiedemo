0ca9f1c878c1d8caac241cf895e159a1c5e88c5c
vinayakjain <vinaaykjain388@gmail.com>
2024-04-27 10:47:46 +0530

    fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/cookie',(req,res)=>{
     let {tokenValue}=req.body;
 
     return res.cookie("token",tokenValue,{
-       sameSite:none,
+       sameSite:"none",
        secure:true,
     }).json({
         message:"cookie initialised",
