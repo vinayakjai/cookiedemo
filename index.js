aeb27754c3a1d4d84926641a06f130174fed87a2
vinayakjain <vinaaykjain388@gmail.com>
2024-04-27 09:48:30 +0530

    cookie backend

---
+++ index.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const cors=require("cors");
+const cookieParser=require("cookie-parser");
+const bodyParser=require("body-parser")
+const app=express();
+
+
+app.use(cors({
+    origin:"https://democookie.netlify.app",
+    credentials:true,
+}))
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.get('/cookie',(req,res)=>{
+    const {token}=req.cookies;
+    console.log(req.cookies);
+    if(!token){
+        return res.json({
+            message:"cookie not present",
+        })
+    }
+
+    return res.json({
+        message:"cookie fetched successfully",
+        token,
+    })
+})
+
+app.post('/cookie',(req,res)=>{
+    let {tokenValue}=req.body;
+
+    return res.cookie("token",tokenValue,{
+        httpOnly:true,
+    }).json({
+        message:"cookie initialised",
+    })
+})
+app.listen(3000,()=>{
+    console.log("server is up");
+})
